e268ca5296ef0a823351b6f071e32b3c0ffe2c11
Jon Waiter <jondwaite@hotmail.com>
2020-06-10 16:23:13 +1200

    Updated README and split APIVersion to own cmdlet

--- Invoke-vCloud.psm1
+++ Invoke-vCloud.psm1
@@ -1,18 +1,21 @@
 ## Invoke-vCloud.psm1 - Wrapper for Invoke-RestMethod for consuming VMware vCloud Director API
 
 # Main function to interact with the vCloud Director REST API from within PowerShell
-# Note: Does not require PowerCLI, but without it you'll need to specify a valid x-vcloud-authorization token to authenticate
-# against the vCloud API.
+# Note: Does not require PowerCLI, but without it you'll need to specify a valid x-vcloud-authorization 
+# or JWT token to authenticate against the vCloud API.
 
 Function Invoke-vCloud(
-[Parameter(Mandatory=$true)][uri]$URI,      # We must specify the API endpoint
-[string]$ContentType,                       # Optional ContentType for returned XML
-[string]$Method = 'GET',                    # HTTP verb to use (default 'GET')
-[string]$ApiVersion,                        # vCloud Director API version
-[string]$Body,                              # Any body document to be submitted to the API
-[int]$Timeout = 40,                         # Timeout in seconds to wait for API response (by default)
-[boolean]$WaitForTask = $false,             # Should we wait for task completion if reponse includes a task href?
-[string]$vCloudToken                        # If not already authenticated using Connect-CIServer (PowerCLI) allow us to specify a token
+    [Parameter(Mandatory=$true)][uri]$URI,      # We must specify the API endpoint
+    [string]$ContentType,                       # Optional ContentType for returned XML
+    [string]$Method = 'GET',                    # HTTP verb to use (default 'GET')
+    [Parameter(Mandatory=$true)][string]$ApiVersion,  # vCloud Director API version (e.g. '34.0')
+    [string]$Body,                              # Any body document to be submitted to the API
+    [int]$APITimeout = 30,                      # Timeout in seconds to wait for API response (by default)
+    [int]$TaskTimeout = 300,                    # Timeout in seconds to wait for a VCD task to complete
+    [switch]$WaitForTask,                       # Should we wait for task completion if reponse includes a task href?
+    [string]$vCloudToken,                       # If not already authenticated using Connect-CIServer (PowerCLI) allow us to specify a token
+    [string]$vCloudJWT,                         # Allow use of JWT token instead of x-vcloud-authorization token
+    [switch]$skipCertificateCheck               # Ignore the SSL certificate presented by the endpoint - Requires PS 7.0+
 )
 {
 <#
@@ -30,15 +33,22 @@ An optional parameter which provides the ContentType of any submitted data.
 An optional parameter to specify the HTML verb to be used (GET, PUT, POST or
 DELETE). Defaults to 'GET' if not specified.
 .PARAMETER ApiVersion
-An optional parameter to specify the API version to be used for the call.
-If not specified a request will be made to https://<host>/api/versions and
-the highest non-deprecated version returned used.
+An required parameter to specify the API version to be used for the call. If
+API version is not known use Get-vCloudAPIVersion to determine this for the
+endpoint first.
 .PARAMETER Body
 An optional parameter which specifies XML body to be submitted to the API
 (usually for a PUT or POST action).
-.PARAMETER Timeout
+.PARAMETER APITimeout
 An optional parameter which specifies the time (in seconds) to wait for an API
-call to complete. Defaults to 40 seconds if not specified.
+call to complete. Defaults to 40 seconds if not specified. Note that this is
+the time for the API to respond and NOT for a task to complete (see 
+the TaskTimeout parameter for that).
+.PARAMETER TaskTimeout
+An optional parameter which specifies the time (in seconds) to wait for a
+Cloud Director task to finish. Tasks can be long-running, especially when
+deploying new VMs/vApps so set this value appropriately if the task is
+expected to take a long time.
 .PARAMETER WaitForTask
 If the API call we submit results in a Task object indicating an asynchronous
 vCloud task, should we wait for this to complete before returning? Defaults to
@@ -48,6 +58,17 @@ An alternative method of passing a session token to Invoke-vCloud if there is
 no current PowerCLI session established to a vCloud endpoint. The session must
 have already been established and be still valid (not timed-out). The value
 supplied is copied to the 'x-vcloud-authorization' header value in API calls.
+.PARAMETER vCloudJWT
+Another alternative method of passing a session token to Invoke-vCloud using
+a Java Web Token (JWT) from an existing session. The session must have already
+been established and be still valid (not timed-out). The value supplied is
+copied to the 'X-VMWARE-VCLOUD-ACCESS-TOKEN' header value in API calls.
+.PARAMETER skipCertificateCheck
+A switch that allows ignoring invalid SSL certificates from the API endpoint.
+Should not be used in production environments. Note that the
+-SkipCertificateCheck option for Invoke-RestMethod is only supported in
+PowerShell versions 6.0 and higher, attempting to use this flag in lower
+versions of PowerShell will result in an error.
 .OUTPUTS
 An XML document returned from the vCloud API (if not waiting for task), or a
 success/failure indication if waiting for a task to complete.
@@ -65,42 +86,44 @@ You must either have an existing PowerCLI connection to vCloud Director
 (using the -vCloudToken parameter) in order to authenticate against the API for
 calls to suceed.
 #>
-    $mySessionID = ($Global:DefaultCIServers | Where-Object { $_.Name -eq $URI.Host }).SessionID
-    if (!$mySessionID) {                    # If we didn't find an existing PowerCLI session for our URI
-        if ($vCloudToken) {                 # But we have been passed a x-vcloud-authorization token, use that
-            $mySessionID = $vCloudToken
-        } else {                            # Otherwise we have no authentication mechanism available so quit
-            Write-Error ("No existing session found and no vCloudToken specified, cannot authenticate, exiting.")
+
+# Build authentication header based on current session or supplied token:
+
+    $mySessionId = ($Global:DefaultCIServers | Where-Object { $_.Name -eq $URI.Host }).SessionId
+
+    if ($mySessionId) {                     # Found a matching session - Connect-CIServer has been used:
+        $Headers = @{'x-vcloud-authorization'=$mySessionId}
+    } else {                                # No connected session found, see if we have a token:
+        if ($vCloudToken) {
+            $Headers = @{'x-vcloud-authorization'=$vCloudToken}
+        } elseif ($vCloudJWT) {
+            $Headers = @{'X-VMWARE-VCLOUD-ACCESS-TOKEN'=$vCloudJWT}
+        } else {
+            Write-Error ("No existing Connect-CIServer session found and no vCloudToken or vCloudJWT token specified.")
+            Write-Error ("Cannot authenticate to the vCloud API, exiting.")
             Return
         }
-    } # $mySessionID not set
-
-    # Determine which API version we should use if none is specified:
-    if (!$ApiVersion) {
-        $ApiUri = "https://$($URI.Host)/api/versions"
-        $Headers = @{"Accept"='application/*+xml'}
-        Try {
-            [xml]$r = Invoke-RestMethod -Method Get -Uri $ApiUri -Headers $Headers -TimeoutSec $Timeout
-        } Catch {
-            Write-Warning ("Invoke-vCloud Exception finding API versions: $($_.Exception.Message)")
-            if ( $_.Exception.ItemName ) { Write-Warning ("Failed Item: $($_.Exception.ItemName)") }
-            Return
-        }
-        $ApiVersion = (($r.SupportedVersions.VersionInfo | Where-Object { $_.deprecated -eq $False }) | Measure-Object -Property Version -Maximum).Maximum.ToString() + ".0"
-
-        Write-Host ("Using API version: $($ApiVersion)")
     }
-    
-    # If ContentType or Body are not specified, remove the variable definitions so they won't get passed to Invoke-RestMethod:
-    if (!$ContentType) { Remove-Variable ContentType }
-    if (!$Body) { Remove-Variable Body }
 
     # Configure HTTP headers for this request:
-    $Headers = @{ "x-vcloud-authorization" = $mySessionID; "Accept" = 'application/*+xml;version=' + $ApiVersion }
+    $Headers.Add("Accept","application/*+xml;version=$($ApiVersion)")
+
+    # Build parameters hash to be passed to Invoke-RestMethod
+    $InvokeParams = @{
+        Method = $Method
+        Uri = $URI
+        Headers = $Headers
+        TimeoutSec = $APITimeout
+    }
+
+    # Handle optional parameters and add to hash where defined:
+    if ($ContentType)          { $InvokeParams.ContentType          = $ContentType }
+    if ($Body)                 { $InvokeParams.Body                 = $Body }
+    if ($skipCertificateCheck) { $InvokeParams.SkipCertificateCheck = $true }
 
     # Submit API request:
     Try {
-        [xml]$response = Invoke-RestMethod -Method $Method -Uri $URI -Headers $Headers -Body $Body -ContentType $ContentType -TimeoutSec $Timeout
+        [xml]$response = Invoke-RestMethod @InvokeParams
     }
     Catch {                                 # Something went wrong, so return error information and exit:
         Write-Warning ("Invoke-vCloud Exception: $($_.Exception.Message)")
@@ -114,8 +137,8 @@ calls to suceed.
             Write-Host ("Task submitted successfully, waiting for result")
             Write-Host ("q=queued, P=pre-running, .=Task Running:")
 
-            while($timeout -gt 0) {
-                $taskxml = Invoke-RestMethod -Uri $response.Task.href -Method 'Get' -Headers $Headers -TimeoutSec 5        # Get/refresh our task status
+            while($TaskTimeout -gt 0) {
+                $taskxml = Invoke-RestMethod -Uri $response.Task.href -Method 'Get' -Headers $Headers -TimeoutSec $APITimeout  # Get/refresh our task status
                 switch ($taskxml.Task.status) {
                     "success" { Write-Host " "; Write-Host "Task completed successfully"; return $true; break }
                     "running" { Write-Host -NoNewline "." }
@@ -125,8 +148,8 @@ calls to suceed.
                     "queued" { Write-Host -NoNewline "q" }
                     "preRunning" { Write-Host -NoNewline "P" }
                 } # switch on current task status
-                $timeout -= 1                                           # Decrease our timeout
-                Start-Sleep -s 1                                        # Pause 1 second
+                $TaskTimeout -= 5                                       # Decrease our Task timeout
+                Start-Sleep -s 5                                        # Pause 5 seconds
             } # Timeout expired
             Write-Warning "Task timeout reached (task may still be in progress)"
             return $false
@@ -140,5 +163,64 @@ calls to suceed.
 
 } # Invoke-vCloud Function end
 
+Function Get-vCloudAPIVersion(
+    [Parameter(Mandatory=$true)][uri]$URI,      # API Endpoint to check the version for
+    [int]$APITimeout = 30,                      # Optional timeout in seconds to wait for API response
+    [switch]$skipCertificateCheck               # Ignore SSL errors (defaults to False)
+)
+{
+<#
+.SYNOPSIS
+Gets the highest supported API version for the specified vCloud endpoint
+.DESCRIPTION
+Get-vCloudAPIVersion provides an easy method to determine the highest
+non-deprecated API version for the URI specified. The format of API versions
+in vCloud Director is a string value (e.g. '33.0') even though all
+currently valid values appear to be floats.
+.PARAMETER URI
+A mandatory parameter which provides the API URI to be accessed.
+.PARAMETER APITimeout
+An optional parameter which specifies the time in seconds to wait for a
+response from the API version call. If this time is exceeded a timeout
+error is returned. Defaults to 30 seconds.
+.PARAMETER skipCertificateCheck
+A switch that allows ignoring invalid SSL certificates from the API endpoint.
+Should not be used in production environments. Note that the
+-SkipCertificateCheck switch for Invoke-RestMethod is only supported in
+PowerShell versions 6.0 and higher, attempting to use this flag in lower
+versions of PowerShell will result in an error.
+.OUTPUTS
+A string containing the highest supported API version for the specified
+URI.
+.EXAMPLE
+Returns the highest supported API version for the specified URI:
+PS /> Get-vCloudAPIVersion -URI https://my.cloud.com/
+
+33.0
+.NOTES
+No existing session is required to the specified URI as the request for
+API versions supported by the endpoint does not require authentication.
+#>
+
+    $InvokeParams = @{
+        Method = 'Get'
+        Uri = "https://$($URI.Host)/api/versions"
+        Headers = @{'Accept'='application/*+xml'}
+        TimeoutSec = $APITimeout
+    }
+    if ($skipCertificateCheck) {
+        $InvokeParams.SkipCertificateCheck = $true
+    }
+
+    Try { [xml]$r = Invoke-RestMethod @InvokeParams }
+    Catch {
+        Write-Warning ("Invoke-vCloud Exception finding API versions: $($_.Exception.Message)")
+        if ( $_.Exception.ItemName ) { Write-Warning ("Failed Item: $($_.Exception.ItemName)") }
+        Return "Error determining highest supported API version."
+    }
+    Return (($r.SupportedVersions.VersionInfo | Where-Object { $_.deprecated -eq $False }) | Measure-Object -Property Version -Maximum).Maximum.ToString() + ".0"
+}
+
 # Export function from module
 Export-ModuleMember -Function Invoke-vCloud
+Export-ModuleMember -Function Get-vCloudAPIVersion
--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # Invoke-vCloud
 PowerShell module to make API calls to vCloud Director easier
+
+Invoke-vCloud contains 2 PowerShell cmdlets to make it easier to interact with VMware Cloud Director (VCD) environments. These can be used by both Service Providers (SPs) and Customers when interacting with the vCloud API.
+
+Each cmdlet is detailed in the sections below.
+
+## Invoke-vCloud cmdlet
+
+Invoke vCloud requires an existing connection to a VMware Cloud Director API endpoint. This can automatically be retrieved if the connection is from VMware PowerCLI (`Connect-CIServer`) or can be manually provided using the `vCloudToken` or `vCloudJWT` parameters.
+
+| Parameter | Mandatory | Type | Default | Description |
+| --------- | --------- | ---- | ------- | ----------- |
+| URI       | Yes       | URI  | -       | The URI for the API call. (e.g. https://my.cloud.com/api/org) |
+| ContentType | No      | String | -     | Optional ContentType for the API body text |
+| Method    | No        | String | Get   | API method type to be called |
+| ApiVersion | Yes      | String | -     | API version to be used, see [Get-vCloudAPIVersion cmdlet](#Get-vCloudAPIVersion-cmdlet) below |
+| Body      | No        | String | -     | The body text to be submitted to the API |
+| APITimeout | No       | Integer | 30   | The time (in seconds) for the API to respond |
+| TaskTimeout | No      | Integer | 300  | The time (in seconds) for an API task to complete if `WaitforTask` is specified and the task is successfully submitted |
+| WaitforTask | No      | Switch | -     | If this switch is specified and the API request results in a long-running operation (e.g. deploy a new VM) then Invoke-vCloud will wait for task completion before returning a true/false result for success/failure. |
+| vCloudToken | No      | String | -     | Specifies a token to be used in the `x-vcloud-authorization` header to API requests for connections which don't already exist (using `Connect-CSIServer`) |
+| vCloudJWT   | No      | String | -     | Specifies a Java Web Token (JWT) to be used in the `X-VMWARE-VCLOUD-ACCESS-TOKEN` header to API requests for connections which don't already exist (using `Connect-CIServer`) |
+| skipCertificateCheck | No | Switch | - | If this switch is specified then no SSL certificate check will be conducted on the API endpoint. This can be useful if working against development/test VCD instances which aren't using trusted SSL certificates. Note that this parameter is only supported in PowerShell version 6.0 up |
+
+### Example
+
+```PowerShell
+PS /> Invoke-vCloud -URI https://my.cloud.com/api/org -ApiVersion '34.0'
+
+
+xml                                             OrgList
+---                                             -------
+version="1.0" encoding="UTF-8" standalone="yes" OrgList
+
+PS /> (Invoke-vCloud -URI https://chc.ccldev.co.nz/api/org -ApiVersion '34.0').OrgList.Org.href
+
+https://my.cloud.com/api/org/a6f8e9f1-a10c-4254-b666-21e1099eff79
+```
+
+## Get-vCloudAPIVersion cmdlet
+
+Get-vCloudAPIVersion retrieves the highest supported API version from the specified VMware Cloud Director API endpoint and returns this as a string. This can then be used in subsequent calls to [Invoke-vCloud](##Invoke-vCloud)
+
+| Parameter | Mandatory | Type | Default | Description |
+| --------- | --------- | ---- | ------- | ----------- |
+| URI       | Yes       | URI  | -       | The URI of the API endpoint (e.g. https://my.cloud.com/) |
+| APITimeout | No       | Integer | 30   | The time (in seconds) for the API to respond |
+| skipCertificateCheck | No | Switch | - | If this switch is specified then no SSL certificate check will be conducted on the API endpoint. This can be useful if working against development/test VCD instances which aren't using trusted SSL certificates. Note that this parameter is only supported in PowerShell version 6.0 up |
+
+### Example
+
+```PowerShell
+Get-vCloudAPIVersion -URI 'https://my.cloud.com'
+
+34.0
+```
+
+Recent API versions are shown in the table below:
+
+## VMware Cloud Director API Versions
+
+| API Version | VMware Cloud Director Version | Released  |
+| ----------- | ----------------------------- | --------  |
+| 27.0        | vCloud Director 8.2           | February 2017 |
+| 29.0        | vCloud Director 9.0           | September 2017 |
+| 30.0        | vCloud Director 9.1           | March 2018 |
+| 31.0        | vCloud Director 9.5           | October 2018 |
+| 32.0        | vCloud Director 9.7           | March 2019 |
+| 33.0        | vCloud Director 10.0          | September 2019 |
+| 34.0        | VMware Cloud Director 10.1    | April 2020 |
